8faf6cb28baff0050f69f687ed240e87e8a230dd
Luke Cavanagh <lukec@firetoss.com>
2017-02-07 10:27:04 -0700

    Update jpeg-compression-options.php

--- jpeg-compression-options.php
+++ jpeg-compression-options.php
@@ -28,13 +28,102 @@ function my_plugin_action_links( $links ) {
 //on-load, sets up the following settings for the plugin
 add_action( 'admin_init', 'jpg_compression_browse_settings' );
 function jpg_compression_browse_settings() {
-	register_setting( 'jpg_compression_display_settings-group', 'jpg_disable_compression' );
-	register_setting( 'jpg_compression_display_settings-group', 'jpg_high_compression' );
-	register_setting( 'jpg_compression_display_settings-group', 'jpg_medium_compression' );
-	register_setting( 'jpg_compression_display_settings-group', 'jpg_low_compression' );
+    register_setting( 'jpg_compression_display_settings-group', 'jpg_compression' );
 }
 
-add_filter('jpeg_quality', create_function('', 'return 100;'));
-add_filter('wp_editor_set_quality', create_function('', 'return 100;'));
+//sets default if no option is set
+if (!get_option('jpg_compression')) {
+       update_option('jpg_compression','jpg_low_compression');
+}
+
+//pulls the option value to use in case statement
+$jpg_compression = get_option('jpg_compression');
+
+switch ($jpg_compression) {
+    case "jpg_disable_compression":
+    $return_val = 100;
+    break;
+    case "jpg_high_compression":
+    $return_val = 70;
+    break;
+    case "jpg_medium_compression":
+    $return_val = 80;
+    break;
+    case "jpg_low_compression":
+    $return_val = 92;
+    break;
+    default:
+    $return_val=92;
+}
+add_filter('jpeg_quality', create_function('', "$return_val"));
+add_filter('wp_editor_set_quality', create_function('', "$return_val"));
+
+//displays the settings page
+function jpg_compression_display_settings() {
+	//first part here displays a form to change the settings
+	echo "<form method=\"post\" action=\"options.php\">";
+	settings_fields( 'jpg_compression_display_settings-group' );
+	do_settings_sections( 'jpg_compression_display_settings-group' );
+    echo "
+	
+	<style>.seperator { border-bottom: 1px solid black; }</style>
+	
+	<div><h1>JPG Compression Options</h1><p>
+An easy way to set the JPG compression in WordPress.
+</ul></p>
+<table class=\"form-table\">
+	<tr><td colspan=\"4\"><h2>Select one of the compression options.</h2></td></tr> 
+       <tr valign=\"top\">
+        <th scope=\"row\">Disable Compression</th>
+        <td><input type=\"radio\" name=\"jpg_compression\" value=\"jpg_disable_compression\" ";
+
+if(get_option(jpg_compression) == 'jpg_disable_compression') { 
+        echo "checked='checked'"; 
+}
+
+echo "/></td>
+<td><p>This will disable compression by setting it to 100.</p></td>
+        </tr>
+
+        <tr valign=\"top\">
+        <th scope=\"row\">High Compression</th>
+        <td><input type=\"radio\" name=\"jpg_compression\" value=\"jpg_high_compression\" ";
+
+if(get_option(jpg_compression) == 'jpg_high_compression') { 
+        echo "checked='checked'"; 
+}
+
+echo "/></td>
+<td><p>This will set the compression at 70.</p></td>
+        </tr>
+		
+	 <tr valign=\"top\">
+        <th scope=\"row\">Medium Compression</th>
+        <td><input type=\"radio\" name=\"jpg_compression\" value=\"jpg_medium_compression\" ";
+
+if(get_option(jpg_compression) == 'jpg_medium_compression') { 
+        echo "checked='checked'"; 
+}
+
+echo "/></td>
+<td><p>This will set the compression at 80.</p></td>
+        </tr></table>
+	
+	<tr valign=\"top\">
+        <th scope=\"row\">Low Compression</th>
+        <td><input type=\"radio\" name=\"jpg_compression\" value=\"jpg_low_compression\" ";
+
+if(get_option(jpg_compression) == 'jpg_low_compression') { 
+        echo "checked='checked'"; 
+}
+
+echo "/></td>
+<td><p>This will set the compression at 92.</p></td>
+       </tr></table>";
+    
+    submit_button();
+	echo "</form><br><br>";
+	echo "</div>";
+}
 
 ?>
